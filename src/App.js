6ece7ce204e18503d4d8c69f92ff2f75146060bf
ryanholly3 <ryanholly3@gmail.com>
2020-04-04 12:37:33 -0600

    Adds material icons, and a custom material theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import NavigationBar from './components/NavigationBar/index.js'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
 import './App.css';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+  },
+})
 
 export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavigationBar />
-      </div>
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <NavigationBar />
+        </div>
+      </ThemeProvider>
     );
   }
 }
--- src/components/NavigationBar/index.js
+++ src/components/NavigationBar/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 export default class NavigationBar extends Component {
   render() {
@@ -8,6 +10,9 @@ export default class NavigationBar extends Component {
       <div className="NavigationBar">
         <AppBar postition='static'>
           <Toolbar>
+            <IconButton aria-label='menu'>
+              <MenuIcon />
+            </IconButton>
           </Toolbar>
         </AppBar>
       </div>
